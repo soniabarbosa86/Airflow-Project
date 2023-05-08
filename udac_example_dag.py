d91c72a49f3d4fb1466d008d4956d57a815f3168
soniabarbosa86 <99013713+soniabarbosa86@users.noreply.github.com>
2023-05-08 13:52:45 +0200

    Update udac_example_dag.py

--- udac_example_dag.py
+++ udac_example_dag.py
@@ -3,13 +3,22 @@ import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator,
-                              PostgresOperator)
+                                LoadDimensionOperator, DataQualityOperator,PostgresOperator)
+                              
 from helpers import SqlQueries
 
 #AWS_KEY = os.environ.get('AWS_KEY')
 #AWS_SECRET = os.environ.get('AWS_SECRET')
+"""
+Default values for the arguments of the DAG:
+- owner: owner of the DAG
+- start_date: start date of the DAG
+- email_on_retry: send or not send an email when a task is retried
+- retries: amount of times a task to run if it fails
+- retry_delay: amount of time between retries
+- depends_on_past : if a task depends on the the previous run was ok.
 
+"""
 default_args = {
     'owner': 'SoniaBarbosa',
     'start_date': datetime(2019, 1, 12),
@@ -19,23 +28,42 @@ default_args = {
     'depends_on_past': False
 }
 
+"""
+creation of the DAG:
+- default_args: inherited values from the above default_args
+- description: describes what the DAG does
+- schedule_interval: specifies the schedule interval for the DAG. For this project the interval is hourly. 
+
+"""
+
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *'
         )
 
+"""
+Dummy Operator used to indicate that that the DAG has begun"
+"""
 start_operator = DummyOperator(
     task_id='Begin_execution',
     dag=dag,
     )
 
-create_tables = PostgresOperator(
-    task_id="create_tables",
-    dag=dag,
-    postgres_conn_id="redshift",
-    sql="create_tables.sql"
-)
+
+"""
+Operator that stages the data from S3 to Redshift. The parameters are:
+- task_id: ID of the task
+- dag: the DAG the task belongs to
+- table: name of the Redshift table where the data will be copied to
+- redshift_conn_id: ID to connect to Redshift
+- aws_credentials_id: connection ID to access the S3 bucket
+- s3_bucket: name of the S3 bucket
+- s3 key: key of the S3 object to copy which is log_data
+- log_json_path: path to the json used to extract data from the log files
+
+"""
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag,
@@ -48,6 +76,10 @@ stage_events_to_redshift = StageToRedshiftOperator(
     
 )
 
+"""
+This operator performs the same steps as the operator above but for the song data
+
+"""
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
     dag=dag,
