44f090db8941bbf1a0bae2a5ded41fb633a429fb
soniabarbosa86 <99013713+soniabarbosa86@users.noreply.github.com>
2023-05-15 10:06:45 +0200

    Update load_fact.py

--- load_fact.py
+++ load_fact.py
@@ -5,15 +5,16 @@ from airflow.utils.decorators import apply_defaults
 
 
 class LoadFactOperator(BaseOperator):
-"""
-The purpose of this operator is to load data into the fact table songplay in Redshift.
-Default parameters of this operator are:
-- redshift_conn_id: The Airflow connection ID for Redshift
-- table: The name of the fact table in Redshift
-- sql_query: The SQL query to use to load the fact table
-- insert_mode: we want the data to be appended to the data in the table
+    
+    """
+    The purpose of this operator is to load data into the fact table songplay in Redshift.
+    Default parameters of this operator are:
+    - redshift_conn_id: The Airflow connection ID for Redshift
+    - table: The name of the fact table in Redshift
+    - sql_query: The SQL query to use to load the fact table
+    - insert_mode: we want the data to be appended to the data in the table
 
-"""
+    """
     
     ui_color = '#F98866'
     @apply_defaults
@@ -24,10 +25,10 @@ Default parameters of this operator are:
                  insert_mode = append,
                  *args, **kwargs):
                  
-  """
-  Passes the default arguments above
+    """
+    Passes the default arguments above
   
-  """
+    """
         
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
@@ -37,12 +38,13 @@ Default parameters of this operator are:
         
         
  
-"""
-The LoadFactOperator will connect to the Redshift cluster and as the insert_mode is append, it will log a message stating that the data will be loaded and it will also run an INSERT INTO command and also a SQL query to select which data to be inserted
 
-"""
         
     def execute(self, context):
+        """
+        The LoadFactOperator will connect to the Redshift cluster and as the insert_mode is append, it will log a message stating that the data will be loaded and it will also run an INSERT INTO command and also a SQL query to select which data to be inserted
+
+        """
         postgres = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         if self.append_data == append:
             self.log.info(f'Load fact table {self.table}')
